30cfb4172fb74e8015cc4520db25ea506e6e367d
Kristian Mandrup <kmandrup@gmail.com>
2010-09-10 14:17:22 +0200

    better delete artifact

--- lib/rails3_artifactor/base/crud/delete.rb
+++ lib/rails3_artifactor/base/crud/delete.rb
@@ -5,7 +5,13 @@ module Rails3::Assist::Artifact::CRUD
     def remove_artifact name, type
       type = type[:type] if type.kind_of? Hash
       file = existing_file_name name, type
-      debug "removed artifact: #{name}" if File.exist?(file) && FileUtils.rm_f(file) 
+      if File.exist?(file)
+        FileUtils.rm_f(file) 
+        debug "removed artifact: #{name}"  
+        true
+      else
+        nil
+      end
     end
     alias_method :delete_artifact, :remove_artifact
 
