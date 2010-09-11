964ccdf2a2a9a4a7df7ff5013fc63371e1af1c4a
Kristian Mandrup <kmandrup@gmail.com>
2010-09-11 12:04:45 +0200

    added options to existing_file_name

--- lib/rails3_artifactor/base/file_name.rb
+++ lib/rails3_artifactor/base/file_name.rb
@@ -9,11 +9,11 @@ module Rails3::Assist::Artifact
       send :"#{type}_file_name", name, options
     end
 
-    def existing_file_name name, type=nil 
+    def existing_file_name name, type=nil, options = {} 
       # first try finder method
       finder_method = :"find_#{type}"       
       if respond_to?(finder_method)          
-        result = send finder_method, name 
+        result = send finder_method, name, options 
         if !result.kind_of? String          
           raise IOError, "The call to #find_#{type}(#{name}) didn't find an existing #{type} file. Error in find expression: #{result.find_expr}" 
         end
@@ -23,15 +23,15 @@ module Rails3::Assist::Artifact
       end
       
       # default for non-migration
-      file_name = make_file_name(name, type)      
+      file_name = make_file_name name, type, options
       raise IOError, "No file for :#{type} found at location: #{file_name}" if !File.file?(file_name)
       file_name
     end
 
     Rails3::Assist.artifacts.each do |name|
       class_eval %{
-        def existing_#{name}_file name, type=nil
-          existing_file_name name, type
+        def existing_#{name}_file name, type=nil, options = {}
+          existing_file_name name, type, options
         end
       } 
     end
