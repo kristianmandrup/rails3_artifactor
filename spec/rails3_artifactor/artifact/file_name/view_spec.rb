3b30dbb177702ff5edd605cf71e33fe22bcde2c3
Kristian Mandrup <kmandrup@gmail.com>
2010-09-14 23:50:22 +0200

    more view file_name specs - all pass

--- spec/rails3_artifactor/artifact/file_name/view_spec.rb
+++ spec/rails3_artifactor/artifact/file_name/view_spec.rb
@@ -15,8 +15,24 @@ describe Rails3::Assist::Artifact::View::FileName do
   end
 
   describe '#view_file_name' do
-    it "should return the file name for the persons/show view" do      
+    it "should return the file name for the persons/show view with default action" do      
+      CLASS.view_file_name(:person).should match 'views/person/show'.to_regexp
+    end
+
+    it "should return the file name for the persons/show view with default type" do      
       CLASS.view_file_name(:person, :show).should match 'views/person/show'.to_regexp
     end
+
+    it "should return the file name for the persons/show view with template type" do      
+      CLASS.view_file_name(:person, :show, 'erb.hml').should match 'views/person/show'.to_regexp
+    end
+
+    it "should return the file name for the persons/show view using hash" do      
+      CLASS.view_file_name(:person, :action => :show, :type => 'erb.hml').should match 'views/person/show'.to_regexp
+    end
+
+    it "should return the file name for the persons/show view with root path" do      
+      CLASS.view_file_name(:person, :action => :show, :type => 'erb.hml', :root_path => Rails3::Assist::Directory.rails_root).should match 'views/person/show'.to_regexp
+    end        
   end
 end
