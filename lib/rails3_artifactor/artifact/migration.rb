4baff9b676ce46e1cf89cf7bbf05ecd35a0a832e
Kristian Mandrup <kmandrup@gmail.com>
2010-09-20 12:16:28 +0200

    updated specs

--- lib/rails3_artifactor/artifact/migration.rb
+++ lib/rails3_artifactor/artifact/migration.rb
@@ -5,7 +5,7 @@ Rails3::Migration::Assist.rails_root_dir = Rails3::Assist::Directory.rails_root
 module Rails3::Assist::Artifact
   module Migration
     include Rails3::Assist::BaseHelper    
-    include Rails::Migration::Assist::ClassMethods
+    include Rails3::Migration::Assist::ClassMethods
     include Rails3::Assist::Artifact::FileName
   end
 end
